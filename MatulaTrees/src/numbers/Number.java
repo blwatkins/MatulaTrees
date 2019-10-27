d30d11ceb88e10a2e7a3b564ab39b6ff0ee2d0d6
Brittni Watkins <blwatkins@smu.edu>
2019-10-27 18:54:14 -0500

    Add instance variables and constructor to Number class.

--- MatulaTrees/src/numbers/Number.java
+++ MatulaTrees/src/numbers/Number.java
@@ -1,4 +1,23 @@
+// Number Class
+// Stores the prime factorization and n-th prime position of some number
+
 package numbers;
 
+import java.util.ArrayList;
+
 public class Number {
+    private int value;
+    private boolean isPrime;
+    private int primeIndex;
+    private ArrayList<Integer> primeFactors;
+
+    public Number(int value) {
+        this.value = value;
+        isPrime = false;
+        primeIndex = -1;
+        primeFactors = null;
+    }
+
+
+
 }
