38dd53f53e7d91897c8adc9ca273a57ef846d410
Brittni Watkins <blwatkins@smu.edu>
2019-10-27 19:02:29 -0500

    Add addNumber functionality to NumberStore

--- MatulaTrees/src/numbers/NumberStore.java
+++ MatulaTrees/src/numbers/NumberStore.java
@@ -1,4 +1,26 @@
+// NumberStore Class
+// Stores a single instance of each number
+
 package numbers;
 
+import java.util.HashMap;
+
 public class NumberStore {
+    private static HashMap<Integer, Number> numbers = new HashMap<>();
+
+    public static void addNumber(int n) {
+        if (!hasNumber(n)) {
+            createNumber(n);
+        }
+    }
+
+    private static boolean hasNumber(int n) {
+        return numbers.containsKey(n);
+    }
+
+    private static void createNumber(int n) {
+        Number number = new Number(n);
+        numbers.put(n, number);
+    }
+
 }
