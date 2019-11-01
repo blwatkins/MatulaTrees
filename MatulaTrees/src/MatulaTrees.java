d2b0b4d0be118edcac933420dbe62fc15dc68717
Brittni Watkins <blwatkins@smu.edu>
2019-11-01 11:51:25 -0500

    Add main processing sketch running code

--- MatulaTrees/src/MatulaTrees.java
+++ MatulaTrees/src/MatulaTrees.java
@@ -1,2 +1,22 @@
-public class MatulaTrees {
+import processing.core.PApplet;
+
+public class MatulaTrees extends PApplet {
+
+    public static void main(String[] args) {
+        String[] processingArgs = {"MatulaTrees"};
+        PApplet.main(processingArgs);
+    }
+
+    public void settings() {
+
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+
 }
