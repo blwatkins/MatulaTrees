e1dd8c4e837f2e536d7d9a22235d29cb22b7ea27
Brittni Watkins <blwatkins@smu.edu>
2019-11-01 12:41:36 -0500

    Add requestPrimeIndex method

--- MatulaTrees/src/primeRequests/PrimeIndexRequester.java
+++ MatulaTrees/src/primeRequests/PrimeIndexRequester.java
@@ -1,4 +1,18 @@
 package primeRequests;
 
-public class PrimeIndexRequester {
+import java.util.ArrayList;
+
+public class PrimeIndexRequester extends PythonScriptRunner {
+
+    public static long requestPrimeIndex(long n) {
+        ArrayList<Long> requestResult = runPythonScript("pi_of_n", n);
+        long result = -1;
+
+        if (requestResult != null) {
+            result = requestResult.get(0);
+        }
+
+        return result;
+    }
+
 }
