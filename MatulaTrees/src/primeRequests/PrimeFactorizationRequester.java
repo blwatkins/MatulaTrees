aff97553602c4c05ce12960aac020e30aed1c0b5
Brittni Watkins <blwatkins@smu.edu>
2019-11-01 12:50:08 -0500

    Complete request prime factors method

--- MatulaTrees/src/primeRequests/PrimeFactorizationRequester.java
+++ MatulaTrees/src/primeRequests/PrimeFactorizationRequester.java
@@ -1,4 +1,16 @@
 package primeRequests;
 
+import java.util.ArrayList;
+
 public class PrimeFactorizationRequester extends PythonScriptRunner {
+
+    public static ArrayList<Long> requestPrimeFactors(long num) {
+        ArrayList<Long> requestResult = runPythonScript("prime_factors_of_n", num);
+
+        if (requestResult == null) {
+            requestResult = new ArrayList<>();
+        }
+
+        return requestResult;
+    }
 }
