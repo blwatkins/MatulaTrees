09bf83b5fd373851062a06e10a53516612d540be
Brittni Watkins <blwatkins@smu.edu>
2019-11-01 12:57:45 -0500

    Add build number method

--- MatulaTrees/src/numbers/Number.java
+++ MatulaTrees/src/numbers/Number.java
@@ -5,24 +5,44 @@ package numbers;
 
 import java.util.ArrayList;
 
+import primeRequests.NthPrimeRequester;
+import primeRequests.PrimeFactorizationRequester;
+import primeRequests.PrimeIndexRequester;
+
 public class Number {
     private long value;
     private boolean isPrime;
     private long primeIndex;
-    private ArrayList<Integer> primeFactors;
+    private ArrayList<Long> primeFactors;
 
     public Number(long value) {
         this.value = value;
         isPrime = false;
         primeIndex = -1;
         primeFactors = null;
+        buildNumber();
     }
 
-    public Number(long value, boolean isPrime, long primeIndex, ArrayList<Integer> primeFactors) {
+    public Number(long value, boolean isPrime, long primeIndex, ArrayList<Long> primeFactors) {
         this.value = value;
         this.isPrime = isPrime;
         this.primeIndex = primeIndex;
         this.primeFactors = primeFactors;
+    }
+
+    private void buildNumber() {
+        long possiblePrimeIndex = PrimeIndexRequester.requestPrimeIndex(value);
+        long nthPrime = NthPrimeRequester.requestNthPrime(possiblePrimeIndex);
+
+        if (value == nthPrime) {
+            isPrime = true;
+            primeIndex = possiblePrimeIndex;
+        } else {
+            isPrime = false;
+            primeIndex = -1;
+        }
+
+        primeFactors = PrimeFactorizationRequester.requestPrimeFactors(value);
     }
 
 }
