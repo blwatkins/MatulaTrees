1ab34f9cf6ad5a9710721ebdbb3e57cdaa9570ab
Brittni Watkins <blwatkins@smu.edu>
2019-11-01 12:13:30 -0500

    Add methods for reading process result

--- MatulaTrees/src/primeRequests/PythonScriptRunner.java
+++ MatulaTrees/src/primeRequests/PythonScriptRunner.java
@@ -1,4 +1,54 @@
 package primeRequests;
 
-public class PythonScriptRunner {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.util.ArrayList;
+
+public abstract class PythonScriptRunner {
+
+    public static ArrayList<Long> runPythonScript(String scriptName, long num) {
+        ArrayList<Long> result = null;
+
+        String command = "python python/" + scriptName + ".py " + num;
+
+        try {
+            Process p = Runtime.getRuntime().exec(command);
+            result = getProcessOutput(p);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return result;
+    }
+
+    private static ArrayList<Long> getProcessOutput(Process p) {
+        ArrayList<Long> result = null;
+
+        try {
+            InputStream inputStream = p.getInputStream();
+            InputStreamReader iStreamReader = new InputStreamReader(inputStream);
+            BufferedReader input = new BufferedReader(iStreamReader);
+            String resultString = input.readLine();
+            result = parseResultString(resultString);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return result;
+    }
+
+    private static ArrayList<Long> parseResultString(String string){
+        ArrayList<Long> result = new ArrayList<>();
+        String[] longs = string.split(" ");
+
+        for (String longString: longs) {
+            Long num = Long.parseLong(longString);
+            result.add(num);
+        }
+
+        return result;
+    }
+
 }
