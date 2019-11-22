a01dbb6a65cad3569412ec2e7780e812d7f0d1aa
Brittni Watkins <blwatkins@smu.edu>
2019-11-22 12:48:57 -0600

    Begin matula tree build process Add create root method and create tree method

--- MatulaTrees/src/matulaTree/MatulaTree.java
+++ MatulaTrees/src/matulaTree/MatulaTree.java
@@ -1,4 +1,19 @@
 package matulaTree;
 
 public class MatulaTree {
+    private Node root;
+    private int order;
+
+    public MatulaTree(long value) {
+        createTree(value);
+    }
+
+    private void createTree(long value) {
+        createRoot(value);
+    }
+
+    private void createRoot(long value) {
+
+    }
+
 }
