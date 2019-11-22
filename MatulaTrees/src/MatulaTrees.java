54aa34346f205ad524b43a6da6d849927ef1c7f0
Brittni Watkins <blwatkins@smu.edu>
2019-11-22 13:30:23 -0600

    Remove testing for NumberStore and Number classes

--- MatulaTrees/src/MatulaTrees.java
+++ MatulaTrees/src/MatulaTrees.java
@@ -1,4 +1,3 @@
-import numbers.Number;
 import processing.core.PApplet;
 
 import numbers.NumberStore;
@@ -11,22 +10,13 @@ public class MatulaTrees extends PApplet {
     }
 
     public void settings() {
-        size(1000, 1000);
+        size(800, 800);
     }
 
     public void setup() {
         NumberStore.loadNumbers(this);
         NumberStore.output();
         System.out.println("-----------------------------");
-        NumberStore.addNumber(2);
-        NumberStore.addNumber(3);
-        NumberStore.addNumber(4);
-        NumberStore.addNumber(5);
-        NumberStore.addNumber(6);
-        NumberStore.addNumber(7);
-        NumberStore.addNumber(8);
-        NumberStore.addNumber(9);
-        NumberStore.addNumber(10);
         NumberStore.output();
         NumberStore.saveNumbers(this);
     }
