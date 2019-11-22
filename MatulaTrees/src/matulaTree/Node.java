491c0d7e201706c68dba742099e5a2cfc83c8367
Brittni Watkins <blwatkins@smu.edu>
2019-11-22 12:53:18 -0600

    Add Node constructor

--- MatulaTrees/src/matulaTree/Node.java
+++ MatulaTrees/src/matulaTree/Node.java
@@ -1,4 +1,18 @@
 package matulaTree;
 
+import java.util.ArrayList;
+
+import numbers.Number;
+
 public class Node {
+    private Number number;
+    private Node parent;
+    private ArrayList<Edge> children;
+
+    public Node(Number number) {
+        this.number = number;
+        parent = null;
+        children = new ArrayList<>();
+    }
+
 }
