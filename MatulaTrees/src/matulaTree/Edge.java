830078422839bf09120db7e121170bb13c4e12a1
Brittni Watkins <blwatkins@smu.edu>
2019-11-22 12:55:01 -0600

    Add Edge constructor

--- MatulaTrees/src/matulaTree/Edge.java
+++ MatulaTrees/src/matulaTree/Edge.java
@@ -1,4 +1,14 @@
 package matulaTree;
 
 public class Edge {
+    private int parentPrimeFactor;
+    private int childPrimeFactor;
+    private Node child;
+
+    public Edge(int parentPrimeFactor) {
+        this.parentPrimeFactor = parentPrimeFactor;
+        childPrimeFactor = 0;
+        child = null;
+    }
+
 }
